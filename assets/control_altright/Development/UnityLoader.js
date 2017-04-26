ac100f54ff172cb9098e3644cf0cfab80f881e55
Mario Maiale <mariosolo@gmail.com>
2017-04-26 19:47:43 -0400

    Game Upload

---
+++ assets/control_altright/Development/UnityLoader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb41226cb0cc3e17c3c4e309a2062dafe529ef89b26c757a8134ddf2d93fb3f7
+size 124394
---
+++ assets/control_altright/TemplateData/UnityProgress.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9b6d7be840ae1af6f1809fd3286b56a8c503edc51a832f3ca8ce6f8fafff9fa2
+size 4512
